45c26edf99e75b6d0ba1ab5cc9c1d0867b9110cb
Aliwoto <woto@kaizoku.cyou>
2023-03-04 23:43:50 +0330

    Add logo_small and its logic to be used in windows os. Add launch.json for a better debug experience on vscode.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- negar_gui/constants.py
+++ negar_gui/constants.py
@@ -1,5 +1,10 @@
 from pathlib import Path
+import platform
 
 __version__ = "0.2.16"
 
-LOGO = (Path(__file__).parent.absolute()/"icons/logo.png").as_posix()
+if platform.system() == 'Windows':
+    LOGO = (":/images/icons/logo_small.png")
+else:
+    LOGO = (Path(__file__).parent.absolute()/"negar_gui/icons/logo.png").as_posix()
+    
