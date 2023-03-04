82e05f75bf12426ab6cb510e40ba54854ca8bb53
Aliwoto <woto@kaizoku.cyou>
2023-03-05 01:35:22 +0330

    Add windows workflow file and build powershell script.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

---
+++ .github/build.ps1
@@ -0,0 +1,4 @@
+$constFile = Get-ChildItem -Path "." -File -Filter "constants.py" -Recurse | Select-Object -First 1
+$versionStr = Get-Content | Where-Object { $_.StartsWith("__version__")  }
+$versionStr = $versionStr.Split("=")[1].Trim().Trim('"')
+PyInstaller -p negar_gui --onefile --windowed --clean --collect-data pyuca --noupx negar_gui/main.py -n "negar-gui-v$versionStr" --add-data "./python-negar/negar/data/untouchable.dat;negar\data" --icon ".\negar_gui\icons\logo.png"
