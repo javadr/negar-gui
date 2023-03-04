8f69900ea644ab18aade2bf5ccd99795540794b2
javad <javadr@gmail.com>
2023-03-05 00:23:39 +0330

    ver update + issue with logo in Linux

--- negar_gui/constants.py
+++ negar_gui/constants.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 import platform
 
-__version__ = "0.2.16"
+__version__ = "0.2.17"
 
 if platform.system() == 'Windows':
     LOGO = (":/images/icons/logo_small.png")
 else:
-    LOGO = (Path(__file__).parent.absolute()/"negar_gui/icons/logo.png").as_posix()
-    
+    LOGO = (Path(__file__).parent.absolute()/"icons/logo.png").as_posix()
+
