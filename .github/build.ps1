016817a1f59e11278360b36f4d06a304ba9ef30b
Aliwoto <woto@kaizoku.cyou>
2023-03-05 02:09:14 +0330

    Just hardcode constants.py file.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- .github/build.ps1
+++ .github/build.ps1
@@ -1,6 +1,6 @@
-$constFile = Get-ChildItem -Path "." -File -Filter "constants.py" -Recurse | Select-Object -First 1
+# $constFile = Get-ChildItem -Path "." -File -Filter "constants.py" -Recurse | Select-Object -First 1
 Get-ChildItem | Write-Host
 
-$versionStr = Get-Content -Path $constFile | Where-Object { $_.StartsWith("__version__")  }
+$versionStr = Get-Content -Path ".\negar_gui\constants.py" | Where-Object { $_.StartsWith("__version__")  }
 $versionStr = $versionStr.Split("=")[1].Trim().Trim('"')
 & "PyInstaller" -p negar_gui --onefile --windowed --clean --collect-data pyuca --noupx negar_gui/main.py -n "negar-gui-v$versionStr" --add-data "./python-negar/negar/data/untouchable.dat;negar\data" --icon ".\negar_gui\icons\logo.png"
