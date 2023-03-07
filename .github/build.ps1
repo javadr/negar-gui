42e1aa2264083ebfc110dc316de37576a2dee6e5
Aliwoto <woto@kaizoku.cyou>
2023-03-08 01:23:50 +0330

    Try to set version in env variables and use it for automatic_release_tag.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- .github/build.ps1
+++ .github/build.ps1
@@ -3,4 +3,5 @@ Get-ChildItem | Write-Host
 
 $versionStr = Get-Content -Path ".\negar_gui\constants.py" | Where-Object { $_.StartsWith("__version__")  }
 $versionStr = $versionStr.Split("=")[1].Trim().Trim('"')
+$Env:CURRENT_VERSION = $versionStr
 & "python" -m PyInstaller -p negar_gui --onefile --windowed --clean --collect-data pyuca --noupx negar_gui/main.py -n "negar-gui-v$versionStr" --add-data "./python-negar/negar/data/untouchable.dat;negar\data" --icon ".\negar_gui\icons\logo.png"
--- .github/build.sh
+++ .github/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 version=$(grep -oE "([0-9]*\.[0-9]*\.[0-9]*)" ./negar_gui/constants.py)
-
+export CURRENT_VERSION="$version"
 pyinstaller -p negar_gui --onefile --windowed --clean -i"negar_gui/icons/logo.ico" \
-	--collect-data pyuca --noupx negar_gui/main.py -n negar-gui-v$(VER) \
+	--collect-data pyuca --noupx negar_gui/main.py -n negar-gui-v$version \
 	--add-data ./python-negar/negar/data/untouchable.dat:negar/data
