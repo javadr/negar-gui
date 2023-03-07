4286ef2c138f716167b98a2a0b4db6b2c67dda88
Aliwoto <woto@kaizoku.cyou>
2023-03-08 03:13:14 +0330

    Delete macos workflow, add some cleanup code to build scripts.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- .github/build.sh
+++ .github/build.sh
@@ -5,3 +5,6 @@ export CURRENT_VERSION="$version"
 pyinstaller -p negar_gui --onefile --windowed --clean -i"negar_gui/icons/logo.ico" \
 	--collect-data pyuca --noupx negar_gui/main.py -n negar-gui-v$version \
 	--add-data ./python-negar/negar/data/untouchable.dat:negar/data
+
+# Cleanup after build
+rm -rf '.\python-negar' 2>&1
