aef0f9b25b379c0a2fe73804ef0f464e690ca79a
javad <javadr@gmail.com>
2023-03-14 18:55:51 +0330

    (fix) issue with RE

--- .github/build.sh
+++ .github/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=$(grep -oE "[0-9]*\.[0-9]*(\.[0-9]*)?" ./negar_gui/constants.py)
+version=$(grep -oE "[0-9]+\.[0-9]+(\.[0-9]+)?" ./negar_gui/constants.py)
 export CURRENT_VERSION="$version"
 pyinstaller -p negar_gui --onefile --windowed --clean -i"negar_gui/icons/logo.ico" \
 	--collect-data pyuca --noupx negar_gui/main.py -n "negar-gui-v$version" \
