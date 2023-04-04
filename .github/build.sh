1e6e1df5a86f3261bc544e550010b0b7b8d3819b
javad <javadr@gmail.com>
2023-04-05 01:34:42 +0330

    (fix) issue with translation in Linux standalone

--- .github/build.sh
+++ .github/build.sh
@@ -5,6 +5,7 @@ export CURRENT_VERSION="$version"
 pyinstaller -p negar_gui --onefile --windowed --clean -i"negar_gui/icons/logo.ico" \
 	--collect-data pyuca --noupx negar_gui/main.py -n "negar-gui-v$version" \
 	--add-data ./python-negar/negar/data/untouchable.dat:negar/data
+	--add-data negar_gui/ts/fa.qm:ts
 
 # Cleanup after build
 rm -rf '.\python-negar' 2>&1
