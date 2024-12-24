7381514beba8d834b35bce04ac08ef0456223b25
javad <javadr@gmail.com>
2024-12-24 01:48:29 +0100

    (upd) workflow

--- .github/build.ps1
+++ .github/build.ps1
@@ -10,7 +10,7 @@ $versionStr = $versionStr.Split("=")[1].Trim('"', ' ')
 $Env:CURRENT_VERSION = $versionStr
 & "python" -m PyInstaller -p negar_gui --onefile --windowed --clean --collect-data pyuca `
     --noupx negar_gui/main.py -n "negar-gui-v$versionStr" `
-    --add-data "./python-negar/negar/data/untouchable.dat;negar\data" `
+    --add-data "./python-negar/negar/data/immutable.words;negar\data" `
     --add-data "negar_gui/ts/fa.qm;ts" `
     --icon ".\negar_gui\icons\logo.png"
 
--- .github/build.sh
+++ .github/build.sh
@@ -4,7 +4,7 @@ version=$(grep -oE "[0-9]+\.[0-9]+(\.[0-9]+)?" ./negar_gui/constants.py)
 export CURRENT_VERSION="$version"
 pyinstaller -p negar_gui --onefile --windowed --clean -i"negar_gui/icons/logo.ico" \
 	--collect-data pyuca --noupx negar_gui/main.py -n "negar-gui-v$version" \
-	--add-data ./python-negar/negar/data/untouchable.dat:negar/data \
+	--add-data ./python-negar/negar/data/immutable.words:negar/data \
 	--add-data negar_gui/ts/fa.qm:ts
 
 chmod +x "dist/negar-gui-v$version"
