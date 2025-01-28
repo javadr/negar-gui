e346b47e821b3e82fe88445b6840ff4b875f984d
javad <javadr@gmail.com>
2025-01-28 21:22:06 +0100

    (upd) .config dir

--- negar_gui/constants.py
+++ negar_gui/constants.py
@@ -1,9 +1,9 @@
 import platform
 from pathlib import Path
 
-__version__ = "0.9.8"
+__version__ = "0.9.9"
 
-APPDATA = "AppData/Roaming/" if platform.system() == "Windows" else "."
+APPDATA = "AppData/Roaming/" if platform.system() == "Windows" else ".config/"
 SETTING_FILE = Path.home() / f"{APPDATA}negar-gui/settings.toml"
 SETTING_FILE.parent.mkdir(parents=True, exist_ok=True)
 
